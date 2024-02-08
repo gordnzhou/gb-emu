9ce12b1f603a65239f38b6e64d8dc99cfb65fb0f
gordnzhou <gordonzhou223@gmail.com>
2024-02-08 15:28:57 -0800

    implement all opcodes except 0xCB bitwise ones

--- src/cpu/sm83.rs
+++ src/cpu/sm83.rs
@@ -10,7 +10,6 @@ pub struct SM83 {
     HL: Register,
     PC: Register,
     SP: Register,
-    stack: Vec<usize>,
     memory: [u8; MEMORY_SIZE],
 }
 
@@ -23,7 +22,6 @@ impl SM83 {
             HL: Register(0),
             PC: Register(0x100),
             SP: Register(0),
-            stack: Vec::new(),
             memory: [0; MEMORY_SIZE],
         }
     }
@@ -34,19 +32,18 @@ impl SM83 {
     // write-only IME flag (handles interrupts)
     // returns the number of clock M-cycles
     pub fn fetch_execute(&mut self) -> u8 {
-        let opcode = self.memory[self.r16("PC")];
+        let opcode = self.memory[self.PC()];
 
         // executes instruction and returns number of clock cycles
         match opcode {
-            // continue this pattern for all opcodes (0x00 to 0xFF)
-            0x00 => 1,
+            0x00 => self.nop(),
             0x01 => self.ld_r16_n16("BC"),
             0x02 => self.ld_r16_a("BC"),
             0x03 => self.inc_r16("BC"),
             0x04 => self.inc_r8("B"),
             0x05 => self.dec_r8("B"),
             0x06 => self.ld_r8_n8("B"),
-            0x07 => 1,
+            0x07 => 0,
             0x08 => self.ld_n16_sp(),
             0x09 => self.add_hl_r16(self.BC()),
             0x0A => self.ld_a_r16("BC"),
@@ -54,26 +51,26 @@ impl SM83 {
             0x0C => self.inc_r8("C"),
             0x0D => self.dec_r8("C"),
             0x0E => self.ld_r8_n8("C"),
-            0x0F => 1,
+            0x0F => 0,
 
-            0x10 => 1,
+            0x10 => self.stop(),
             0x11 => self.ld_r16_n16("DE"),
             0x12 => self.ld_r16_a("DE"),
             0x13 => self.inc_r16("DE"),
             0x14 => self.inc_r8("D"),
             0x15 => self.dec_r8("D"),
             0x16 => self.ld_r8_n8("D"),
-            0x17 => 1,
-            0x18 => 1,
-            0x19 => self.add_hl_r16(self.r16("DE")),
+            0x17 => 0,
+            0x18 => self.jr_e8(),
+            0x19 => self.add_hl_r16(self.DE()),
             0x1A => self.ld_a_r16("DE"),
             0x1B => self.dec_r16("DE"),
             0x1C => self.inc_r8("E"),
             0x1D => self.dec_r8("E"), 
             0x1E => self.ld_r8_n8("E"),
-            0x1F => 1,
+            0x1F => 0,
 
-            0x20 => 1,
+            0x20 => self.jr_cc_e8(self.cc_NZ()),
             0x21 => self.ld_r16_n16("HL"),
             0x22 => self.ld_hli_a(),
             0x23 => self.inc_r16("HL"),
@@ -81,7 +78,7 @@ impl SM83 {
             0x25 => self.dec_r8("H"),
             0x26 => self.ld_r8_n8("H"),
             0x27 => self.daa(),
-            0x28 => 1,
+            0x28 => self.jr_cc_e8(self.cc_Z()),
             0x29 => self.add_hl_r16(self.HL()),
             0x2A => self.ld_a_hli(),
             0x2B => self.dec_r16("HL"),
@@ -90,7 +87,7 @@ impl SM83 {
             0x2E => self.ld_r8_n8("L"),
             0x2F => self.cpl(),
             
-            0x30 => 1,
+            0x30 => self.jr_cc_e8(self.cc_NC()),
             0x31 => self.ld_r16_n16("SP"),
             0x32 => self.ld_hld_a(),
             0x33 => self.inc_r16("SP"),
@@ -98,7 +95,7 @@ impl SM83 {
             0x35 => self.dec_hl(),
             0x36 => self.ld_hl_n8(),
             0x37 => self.scf(),
-            0x38 => 1,
+            0x38 => self.jr_cc_e8(self.cc_C()),
             0x39 => self.add_hl_r16(self.SP()),
             0x3A => self.ld_a_hld(),
             0x3B => self.dec_r16("SP"),
@@ -164,7 +161,7 @@ impl SM83 {
             0x73 => self.ld_hl_r8("E"),
             0x74 => self.ld_hl_r8("H"),
             0x75 => self.ld_hl_r8("L"),
-            0x76 => 1, // HALT
+            0x76 => self.halt(),
             0x77 => self.ld_hl_r8("A"),
             0x78 => self.ld_r8_r8("A", "B"),
             0x79 => self.ld_r8_r8("A", "C"),
@@ -243,116 +240,209 @@ impl SM83 {
             0xBE => self.cp_a_hl(),
             0xBF => self.cp_a_r8(self.A()),
 
-            0xC0 => 1,
+            0xC0 => self.ret_cc(self.cc_NZ()),
             0xC1 => self.pop_r16("BC"),
-            0xC2 => 1,
-            0xC3 => 1,
-            0xC4 => 1,
+            0xC2 => self.jp_cc_n16(self.cc_NZ()),
+            0xC3 => self.jp_n16(),
+            0xC4 => self.call_cc_n16(self.cc_NZ()),
             0xC5 => self.push_r16("BC"),
             0xC6 => self.add_a_n8(),
-            0xC7 => 1,
-            0xC8 => 1,
-            0xC9 => 1,
-            0xCA => 1,
-            0xCB => 1, // CB OPCODES
-            0xCC => 1,
-            0xCD => 1,
+            0xC7 => self.rst(0x00),
+            0xC8 => self.ret_cc(self.cc_Z()),
+            0xC9 => self.ret(),
+            0xCA => self.jp_cc_n16(self.cc_Z()),
+            0xCB => self.cb_execute(),
+            0xCC => self.call_cc_n16(self.cc_Z()),
+            0xCD => self.call_n16(),
             0xCE => self.adc_a_n8(),
-            0xCF => 1,
+            0xCF => self.rst(0x08),
 
-            0xD0 => 1,
+            0xD0 => self.ret_cc(self.cc_NC()),
             0xD1 => self.pop_r16("DE"),
-            0xD2 => 1,
-            0xD3 => 1,
-            0xD4 => 1,
+            0xD2 => self.jp_cc_n16(self.cc_NC()),
+            // 0xD3 => _,
+            0xD4 => self.call_cc_n16(self.cc_NC()),
             0xD5 => self.push_r16("DE"),
             0xD6 => self.sub_a_n8(),
-            0xD7 => 1,
-            0xD8 => 1,
-            0xD9 => 1,
-            0xDA => 1,
-            0xDB => 1,
-            0xDC => 1,
-            0xDD => 1,
+            0xD7 => self.rst(0x10),
+            0xD8 => self.ret_cc(self.cc_C()),
+            0xD9 => self.reti(),
+            0xDA => self.jp_cc_n16(self.cc_C()),
+            // 0xDB => _,
+            0xDC => self.call_cc_n16(self.cc_C()),
+            // 0xDD => _,
             0xDE => self.sbc_a_n8(),
-            0xDF => 1,
+            0xDF => self.rst(0x18),
 
             0xE0 => self.ldh_n16_a(),
             0xE1 => self.pop_r16("HL"),
             0xE2 => self.ldh_c_a(),
-            0xE3 => 1,
-            0xE4 => 1,
+            // 0xE3 => _,
+            // 0xE4 => _,
             0xE5 => self.push_r16("HL"),
             0xE6 => self.and_a_n8(),
-            0xE7 => 1,
+            0xE7 => self.rst(0x20),
             0xE8 => self.add_sp_e8(),
-            0xE9 => 1,
+            0xE9 => self.jp_hl(),
             0xEA => self.ld_n16_a(),
-            0xEB => 1,
-            0xEC => 1,
-            0xED => 1,
+            // 0xEB => _,
+            // 0xEC => _,
+            // 0xED => _,
             0xEE => self.xor_a_n8(),
-            0xEF => 1,
+            0xEF => self.rst(0x28),
             
             0xF0 => self.ldh_a_n16(),
             0xF1 => self.pop_af(),
             0xF2 => self.ldh_a_c(),
-            0xF3 => 1,
-            0xF4 => 1,
+            0xF3 => self.di(),
+            // 0xF4 => _,
             0xF5 => self.push_af(),
             0xF6 => self.or_a_n8(),
-            0xF7 => 1,
+            0xF7 => self.rst(0x30),
             0xF8 => self.ld_hl_sp_e8(),
             0xF9 => self.ld_sp_hl(),
             0xFA => self.ld_a_n16(),
-            0xFB => 1,
-            0xFC => 1,
-            0xFD => 1,
+            0xFB => self.ei(),
+            // 0xFC => _,
+            // 0xFD => _,
             0xFE => self.cp_a_n8(),
-            0xFF => 1,
+            0xFF => self.rst(0x38 ),
+            _ => panic!("Unknown Opcode Encountered! {:#03x}", opcode),
         }
+    }
+
+    fn nop(&mut self) -> u8 {
+        self.inc_PC(1);
+        1
+    }
+
+    fn stop(&mut self) -> u8 {
+        let _ = self.n8();
+        self.inc_PC(2);
+        2
+    }
+
+    // TODO
+    fn di(&mut self) -> u8 {
+        self.inc_PC(1);
+        1
+    }
+
+    // TODO
+    fn ei(&mut self) -> u8 {
+        self.inc_PC(1);
+        1
+    }
+
+    // TODO
+    fn halt(&mut self) -> u8 {
+        self.inc_PC(1);
+        1
+    }
+
+    // TODO
+    fn reti(&mut self) -> u8 {
+        self.inc_PC(1);
+        4
+    }
+
+    fn jp_n16(&mut self) -> u8 {
+        self.set_PC(self.n16());
+        4
+    }
+
+    fn jp_cc_n16(&mut self, cc : bool) -> u8 {
+        if cc { self.jp_n16(); 4 }
+        else { 3 }
+    }
+
+    fn jp_hl(&mut self) -> u8 {
+        self.set_PC(self.HL());
+        1
+    }
+
+    fn jr_e8(&mut self) -> u8 {
+        let mut pc = self.PC() as i16;
+        pc = pc.wrapping_add(self.e8() as i16);
+        self.set_PC(pc as usize);
+        3
+    }
+
+    fn jr_cc_e8(&mut self, cc: bool) -> u8 {
+        if cc { self.jr_e8(); 3 }
+        else { 2 }
+    }
+
+    fn rst(&mut self, vec: usize) -> u8 {
+        self.push_stack(self.PC());
+        self.set_PC(vec);
+        4
+    }
+
+    fn call_n16(&mut self) -> u8 {
+        self.push_stack(self.PC());
+        self.set_PC(self.n16());
+        6
+    }
+
+    fn call_cc_n16(&mut self, cc: bool) -> u8 {
+        if cc { self.call_n16(); 6 }
+        else { 3 }
+    }
+
+    fn ret(&mut self) -> u8 {
+        let res = self.pop_stack();
+        self.set_PC(res);
+        4
+    }
+
+    fn ret_cc(&mut self, cc: bool) -> u8 {
+        if cc { self.ret(); 5 }
+        else { 2 }
     }
 
     fn pop_af(&mut self) -> u8 {
         let res = self.pop_stack();
-        self.AF.set_lo(res.0);
-        self.set_A(res.1);
+        self.AF.set(res);
         self.inc_PC(1);
         3
     }
 
     fn pop_r16(&mut self, r16_name: &str) -> u8 {
         let res = self.pop_stack();
-        self.set_r16(r16_name, (res.1 as usize >> 8) + res.0 as usize);
+        self.set_r16(r16_name, res);
         self.inc_PC(1);
         3
     }
 
-    fn pop_stack(&mut self) -> (u8, u8) {
-        let sp = self.SP();
-        let l = self.get_memory(sp);
-        let r = self.get_memory(sp.wrapping_add(1));
-        self.set_SP(sp.wrapping_add(2));
-        (l, r)
-    }
-
     fn push_af(&mut self) -> u8 {
-        self.push_stack(self.AF.lo(), self.A());
+        self.push_stack(self.AF());
         self.inc_PC(1);
         4
     }
 
     fn push_r16(&mut self, r16_name: &str) -> u8 {
         let r16 = self.r16(r16_name);
-        self.push_stack((r16 & 0xFF00) as u8 >> 8, r16 as u8 & 0xFF);
+        self.push_stack(r16);
         self.inc_PC(1);
         4
     }
 
-    fn push_stack(&mut self, l: u8, r: u8) {
+    fn pop_stack(&mut self) -> usize {
         let sp = self.SP();
-        self.set_memory(sp.wrapping_sub(1), r);
-        self.set_memory(sp.wrapping_add(2), l);
+        let lo = self.get_memory(sp);
+        let hi = self.get_memory(sp.wrapping_add(1));
+        self.set_SP(sp.wrapping_add(2));
+        ((hi as usize) << 8) + lo as usize
+    }
+
+    fn push_stack(&mut self, val16: usize) {
+        let hi = ((val16 & 0xFF00) >> 8) as u8;
+        let lo = val16 as u8;
+        let sp = self.SP();
+
+        self.set_memory(sp.wrapping_sub(1), hi);
+        self.set_memory(sp.wrapping_add(2), lo);
         self.set_SP(sp.wrapping_sub(2));
     }
 
@@ -823,6 +913,291 @@ impl SM83 {
         r as u8
     }
 
+    fn cb_execute(&mut self, ) -> u8 {
+        let opcode = self.memory[self.PC().wrapping_add(1)];
+
+        match opcode {
+            // replace all 1 answers with a '{}' like 0x00 and 0x01 and 0x02
+            0x00 => {},
+            0x01 => {},
+            0x02 => {},
+            0x03 => {},
+            0x04 => {},
+            0x05 => {},
+            0x06 => {},
+            0x07 => {},
+            0x08 => {},
+            0x09 => {},
+            0x0A => {},
+            0x0B => {},
+            0x0C => {},
+            0x0D => {},  
+            0x0E => {},
+            0x0F => {},
+
+            0x10 => {},
+            0x11 => {},
+            0x12 => {},
+            0x13 => {},
+            0x14 => {},
+            0x15 => {},
+            0x16 => {},
+            0x17 => {},
+            0x18 => {},
+            0x19 => {},
+            0x1A => {},
+            0x1B => {},
+            0x1C => {},
+            0x1D => {}, 
+            0x1E => {},
+            0x1F => {},
+
+            0x20 => {},
+            0x21 => {},
+            0x22 => {},
+            0x23 => {},
+            0x24 => {},
+            0x25 => {},
+            0x26 => {},
+            0x27 => {},
+            0x28 => {},
+            0x29 => {},
+            0x2A => {},
+            0x2B => {},
+            0x2C => {},
+            0x2D => {},
+            0x2E => {},
+            0x2F => {},
+            
+            0x30 => {},
+            0x31 => {},
+            0x32 => {},
+            0x33 => {},
+            0x34 => {},
+            0x35 => {},
+            0x36 => {},
+            0x37 => {},
+            0x38 => {},
+            0x39 => {},
+            0x3A => {},
+            0x3B => {},
+            0x3C => {},
+            0x3D => {},
+            0x3E => {},
+            0x3F => {},
+
+            0x40 => {},
+            0x41 => {},
+            0x42 => {},
+            0x43 => {},
+            0x44 => {},
+            0x45 => {},
+            0x46 => {},
+            0x47 => {},
+            0x48 => {},
+            0x49 => {},
+            0x4A => {},
+            0x4B => {},
+            0x4C => {},
+            0x4D => {},
+            0x4E => {},
+            0x4F => {},
+
+            0x50 => {},
+            0x51 => {},
+            0x52 => {},
+            0x53 => {},
+            0x54 => {},
+            0x55 => {},
+            0x56 => {},
+            0x57 => {},
+            0x58 => {},
+            0x59 => {},
+            0x5A => {},
+            0x5B => {},
+            0x5C => {},
+            0x5D => {},
+            0x5E => {},
+            0x5F => {},
+
+            0x60 => {},
+            0x61 => {},
+            0x62 => {},
+            0x63 => {},
+            0x64 => {},
+            0x65 => {},
+            0x66 => {},
+            0x67 => {},
+            0x68 => {},
+            0x69 => {},
+            0x6A => {},
+            0x6B => {},
+            0x6C => {},
+            0x6D => {},
+            0x6E => {},
+            0x6F => {},
+
+            0x70 => {},
+            0x71 => {},
+            0x72 => {},
+            0x73 => {},
+            0x74 => {},
+            0x75 => {},
+            0x76 => {},
+            0x77 => {},
+            0x78 => {},
+            0x79 => {},
+            0x7A => {},
+            0x7B => {},
+            0x7C => {},
+            0x7D => {},
+            0x7E => {},
+            0x7F => {},
+
+            0x80 => {},
+            0x81 => {},
+            0x82 => {},
+            0x83 => {},
+            0x84 => {},
+            0x85 => {},
+            0x86 => {},
+            0x87 => {},
+            0x88 => {},
+            0x89 => {},
+            0x8A => {},
+            0x8B => {},
+            0x8C => {},
+            0x8D => {},
+            0x8E => {},
+            0x8F => {},
+
+            0x90 => {},
+            0x91 => {},
+            0x92 => {},
+            0x93 => {},
+            0x94 => {},
+            0x95 => {},
+            0x96 => {},
+            0x97 => {},
+            0x98 => {},
+            0x99 => {},
+            0x9A => {},
+            0x9B => {},
+            0x9C => {},
+            0x9D => {},
+            0x9E => {},
+            0x9F => {},
+
+            0xA0 => {},
+            0xA1 => {},
+            0xA2 => {},
+            0xA3 => {},
+            0xA4 => {},
+            0xA5 => {},
+            0xA6 => {},
+            0xA7 => {},
+            0xA8 => {},
+            0xA9 => {},
+            0xAA => {},
+            0xAB => {},
+            0xAC => {},
+            0xAD => {},
+            0xAE => {},
+            0xAF => {},
+            
+            0xB0 => {},
+            0xB1 => {},
+            0xB2 => {},
+            0xB3 => {},
+            0xB4 => {},
+            0xB5 => {},
+            0xB6 => {},
+            0xB7 => {},
+            0xB8 => {},
+            0xB9 => {},
+            0xBA => {},
+            0xBB => {},
+            0xBC => {},
+            0xBD => {},
+            0xBE => {},
+            0xBF => {},
+
+            0xC0 => {},
+            0xC1 => {},
+            0xC2 => {},
+            0xC3 => {},
+            0xC4 => {},
+            0xC5 => {},
+            0xC6 => {},
+            0xC7 => {},
+            0xC8 => {},
+            0xC9 => {},
+            0xCA => {},
+            0xCB => {},
+            0xCC => {},
+            0xCD => {},
+            0xCE => {},
+            0xCF => {},
+
+            0xD0 => {},
+            0xD1 => {},
+            0xD2 => {},
+            0xD3 => {},
+            0xD4 => {},
+            0xD5 => {},
+            0xD6 => {},
+            0xD7 => {},
+            0xD8 => {},
+            0xD9 => {},
+            0xDA => {},
+            0xDB => {},
+            0xDC => {},
+            0xDD => {},
+            0xDE => {},
+            0xDF => {},
+
+            0xE0 => {},
+            0xE1 => {},
+            0xE2 => {},
+            0xE3 => {},
+            0xE4 => {},
+            0xE5 => {},
+            0xE6 => {},
+            0xE7 => {},
+            0xE8 => {},
+            0xE9 => {},
+            0xEA => {},
+            0xEB => {},
+            0xEC => {},
+            0xED => {},
+            0xEE => {},
+            0xEF => {},
+            
+            0xF0 => {},
+            0xF1 => {},
+            0xF2 => {},
+            0xF3 => {},
+            0xF4 => {},
+            0xF5 => {},
+            0xF6 => {},
+            0xF7 => {},
+            0xF8 => {},
+            0xF9 => {},
+            0xFA => {},
+            0xFB => {},
+            0xFC => {},
+            0xFD => {},
+            0xFE => {},
+            0xFF => {},
+        }
+
+        self.inc_PC(2);
+        if (opcode & 0xF)!= 0x6 && (opcode & 0xF) != 0xE { 2 }
+        else { 
+            if let 0x4..=0x7 = (opcode & 0xF0) >> 4 { 3 }
+            else { 4 }
+        }
+    }
 
     fn n8(&self) -> u8 {
         self.get_memory(self.PC().wrapping_add(1))
@@ -917,16 +1292,6 @@ impl SM83 {
         }
     }
 
-    fn set_flag(&mut self, name: &str, val: bool) {
-        match name {
-            "Z" => { self.AF.set_bit(7, val); },
-            "N" => { self.AF.set_bit(6, val); },
-            "H" => { self.AF.set_bit(5, val); },
-            "C" => { self.AF.set_bit(4, val); },
-            _ => {}
-        }
-    }
-
     // Register and Flag Getters
     fn A(&self) -> u8 { self.AF.hi() }
     fn B(&self) -> u8 { self.BC.hi() }
@@ -948,15 +1313,16 @@ impl SM83 {
 
     // Register and Flag Setters
     fn set_A(&mut self, val: u8) { self.AF.set_hi(val); }
-    fn set_B(&mut self, val: u8) { self.BC.set_hi(val); }
-    fn set_C(&mut self, val: u8) { self.BC.set_lo(val); }
-    fn set_D(&mut self, val: u8) { self.DE.set_hi(val); }
-    fn set_E(&mut self, val: u8) { self.DE.set_lo(val); }
-    fn set_H(&mut self, val: u8) { self.HL.set_hi(val); }
-    fn set_L(&mut self, val: u8) { self.HL.set_lo(val); }
-    fn set_BC(&mut self, val: usize) { self.BC.set(val); }
-    fn set_DE(&mut self, val: usize) { self.DE.set(val); }
+    // fn set_B(&mut self, val: u8) { self.BC.set_hi(val); }
+    // fn set_C(&mut self, val: u8) { self.BC.set_lo(val); }
+    // fn set_D(&mut self, val: u8) { self.DE.set_hi(val); }
+    // fn set_E(&mut self, val: u8) { self.DE.set_lo(val); }
+    // fn set_H(&mut self, val: u8) { self.HL.set_hi(val); }
+    // fn set_L(&mut self, val: u8) { self.HL.set_lo(val); }
+    // fn set_BC(&mut self, val: usize) { self.BC.set(val); }
+    // fn set_DE(&mut self, val: usize) { self.DE.set(val); }
     fn set_HL(&mut self, val: usize) { self.HL.set(val); }
+    fn set_PC(&mut self, val: usize) { self.PC.set(val); }
     fn inc_PC(&mut self, val: usize) { self.PC.set(self.PC() + val); }
     fn set_SP(&mut self, val: usize) { self.SP.set(val); }
     fn set_cflag(&mut self, val: bool) { self.AF.set_bit(4, val); }
